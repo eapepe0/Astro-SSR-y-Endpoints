87a2da3d50c354aa81f6ba14700812fe8f3f8154
eapepe0 <eapepe0@gmail.com>
2025-01-06 00:48:10 -0300

    Hacemos que la api al recibir un /posts/first-post devuelva el post , esto de la url es llamado segmento de ruta.
    Es básicamente lo mismo que en capitulo anterior , nada mas que en los params recibimos el slug
    
    El problema es que al ser un renderizado estatico , getStaticPaths tirara un error por que son rutas dinámicas.
    
    Para solucionar eso , agregamos el prerender en false.

---
+++ src/pages/api/posts/[slug].ts
@@ -0,0 +1,34 @@
+import type { APIRoute } from "astro";
+import { getCollection, getEntry } from "astro:content";
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request }) => {
+    // en los params vienen los parámetros de la URL
+    // en el request vienen los datos de la petición
+
+    const {slug} = params;
+
+    if(slug){ // si existe el slug en la url
+        // obtenemos el post con el slug
+        const post = await getEntry("blog", slug); 
+        if(post){ // si existe el post
+            return new Response(JSON.stringify(post), { 
+                status: 200 , headers: { "Content-Type": "application/json" },
+            });
+        }
+    // si el post no existe    
+    return new Response(JSON.stringify({
+        msg : 'Error 404 Not found'}), {
+            status: 404 , headers: { "Content-Type": "application/json" }, 
+        });
+        
+    }
+
+
+    // devuelve una respuesta
+    const blogPosts = await getCollection("blog");
+    return new Response(JSON.stringify(blogPosts), {
+        status: 200 , headers: { "Content-Type": "application/json" },
+    });
+}
