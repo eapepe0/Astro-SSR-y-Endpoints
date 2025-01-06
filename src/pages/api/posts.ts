3321facc6bb93090e52cd7ab2048db616c8f4baf
eapepe0 <eapepe0@gmail.com>
2025-01-06 00:07:39 -0300

    Hacemos que la API al tener un query que sea slug, busque en nuestros posts. al no ser enviado el slug envia todos los posts

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,12 +1,38 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
     // en los params vienen los parámetros de la URL
     // en el request vienen los datos de la petición
-    const blogPosts = await getCollection("blog");
+
+    // generamos un objeto URL con la URL de la petición
+    const url = new URL (request.url)
+
+    // extraemos el slug de los parámetros de la URL
+    const slug = url.searchParams.get("slug");
+
+    if(slug){ // si existe el slug en la url
+        // obtenemos el post con el slug
+        const post = await getEntry("blog", slug); 
+        if(post){ // si existe el post
+            return new Response(JSON.stringify(post), { 
+                status: 200 , headers: { "Content-Type": "application/json" },
+            });
+        }
+    // si el post no existe    
+    return new Response(JSON.stringify({
+        msg : 'Error 404 Not found'}), {
+            status: 404 , headers: { "Content-Type": "application/json" }, 
+        });
+        
+    }
+
+
     // devuelve una respuesta
-   return new Response(JSON.stringify(blogPosts), { status: 200 , headers: { "Content-Type": "application/json" }, });
+    const blogPosts = await getCollection("blog");
+    return new Response(JSON.stringify(blogPosts), {
+        status: 200 , headers: { "Content-Type": "application/json" },
+    });
 }
