bc6819e457a6fe5d1af2cdc63d8960e1c3e9d8f8
eapepe0 <eapepe0@gmail.com>
2025-01-06 22:34:37 -0300

    Definimos la tabla en /db/config.ts Usando el defineTable y con el objeto columns vamos definiendo los tipos de datos dentro de defineDb , ponemos que tablas usaremos.
    En nuestro seed.ts, dentro de seed definimos un console.log para ver cada vez que se executa.
    Insertamos los valores con un db.insert(<Tabla>).values([])

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,26 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+// Define la tabla Clients
+const Clients = defineTable({
+  // Define las columnas de la tabla
+  columns: {
+    // el id sera un numero autoincremental unico
+    id : column.number({primaryKey: true}),
+    // el nombre sera un string
+    name : column.text(),
+    // la edad sera un numero
+    age : column.number(),
+    // isActive sera un booleano
+    isActive : column.boolean(),
+  }
+})
+
+
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  // Define las tablas de la base de datos
+  tables: {
+    Clients,
+  }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,20 @@
-import { db } from 'astro:db';
+import { db , Clients } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	
+	// Inserta datos en la tabla Clients
+	await db.insert(Clients).values([
+		{id : 1, name : 'Cristian', age : 38, isActive : true},
+		{id : 2, name : 'Manuel', age : 12, isActive : false},
+		{id : 3, name : 'Florencia', age : 18, isActive : true},
+		{id : 4, name : 'Pepe', age : 27, isActive : true},
+		{id : 5, name : 'Jaime', age : 42, isActive : false},
+		{id : 6, name : 'Mauricio', age : 56, isActive : false},
+		{id : 7, name : 'Rosa', age : 5, isActive : true},
+		{id : 8, name : 'Jose Jose', age : 70, isActive : true},
+
+	])
+
+	console.log('Seed done');
 }
