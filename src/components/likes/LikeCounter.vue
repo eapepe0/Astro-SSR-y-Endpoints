98982ae90bae80092a25c4b1947fda08b10ee78e
eapepe0 <eapepe0@gmail.com>
2025-01-13 20:21:22 -0300

    -  Al generarse el sitio debemos pasarle el postId al componente del boton que es dinamico  ,pero la compilacion cuando generamos la pagina no. Por eso debemos pasar un prop.
     - Variables reactivas , use el v-if / v-else v-else-if que es como un renderizado condicional.
     - Cambiamos el valor de una variable con  el .value
     - En Vue el script se ejecuta una sola vez , por lo cual al final del script podemos hacer un fetch.

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -1,11 +1,37 @@
 <template>
-<button @click="likePost">
-    Like Counter
-</button>
+
+    <div v-if="isLoading">
+        Loading ...
+    </div>
+
+    <button v-else-if="likeCount === 0" @click="likePost">
+        Like this post
+    </button>
+    <button v-else @click="likePost">
+        Likes
+        <span>{{ likeCount }}</span>
+    </button>
 
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+
+// definimos una interfaz en el cual nuestras Props es un postId que es un string
+interface Props{
+    postId : string;
+}
+
+// le decimos a Vue que la props reciben algo del tipo Props
+const props = defineProps<Props>();
+
+// declaramos tres variables reactivas
+
+const likeCount = ref(0) ; // cuenta cuantos likes tiene el post 
+const likeClicks = ref(0) ; // cuantas veces hicimos clicks
+const isLoading = ref(true) ; // si ya termino de cargar 
+
 
 // funcion
 
@@ -13,7 +39,28 @@ const likePost = () => {
     console.log('+1 like')
 }
 
+const getCurrentLikes = async() => {
+    // hago la llamada a la api pasandole el post
+    const resp = await fetch(`/api/posts/likes/${props.postId}`)
 
+    // si la respuesta no es buena no hacemos nada
+    if(!resp.ok) return
+
+    // si la respuesta es buen
+    const data = await resp.json()
+    
+    // el valor de el contador inicialmente es que nos da la api
+    likeCount.value = data.likes;
+
+    // dejamos de cargar , podemos mostrar el boton de Likes
+    isLoading.value = false;
+
+   
+}
+
+
+
+getCurrentLikes()
 </script>
 
 
